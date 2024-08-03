09a300b840b5f0f5a5e6df6b9798e69b4b363c6f
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-08-04 00:16:20 +0530

    tested routes

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const connectDB = require("./database/connectDB");
+const userRoutes = require('./routes/user.routes.js');
+
+const bookingRoutes = require('./routes/booking.routes.js');
 
 const app = express();
 
 PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
+
+
+app.use('/api/users', userRoutes);
+app.use('/api/bookings', bookingRoutes);
+
 
 connectDB()
   .then(
